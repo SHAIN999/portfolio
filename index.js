8a8cd1cba6d60dcbdc447b3cdeb622188eaef56b
ShainMhrz <127409613+ShainMhrz@users.noreply.github.com>
2023-04-21 18:51:18 +0545

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelectorAll('.list');
-list.forEach((list) =>
-list.addEventListener('click', activeLink)); 
-
-function activeLink(){
-list.forEach((list) =>
-list.classList.remove('active'));
-this.classList.add('active')
-}
